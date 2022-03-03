b72760044bbfa028f0663e915f0116c09e04206b
Gijs van der Heide <G.vanderHeide-1@student.tudelft.nl>
2022-03-03 19:21:39 +0100

    Updated commons tests
    The Question and Activity classes went through quite a few changes for the API update, so their tests have now been updated as well.

--- commons/src/test/java/commons/ActivityTest.java
+++ commons/src/test/java/commons/ActivityTest.java
@@ -10,13 +10,13 @@ class ActivityTest {
 
     @Test
     void testEmptyConstructor() {
-        var activity = new Activity();
+        Activity activity = new Activity();
         assertNotNull(activity);
     }
 
     @Test
     void testConstructor() {
-        var activity = new Activity("1","/path/to/image/","Activity", 200);
+        Activity activity = new Activity("1","/path/to/image/","Activity", 200);
         assertNotNull(activity);
         assertEquals("1", activity.id);
         assertEquals("/path/to/image/", activity.imagePath);
@@ -27,36 +27,35 @@ class ActivityTest {
 
     @Test
     void displayActivity() {
-        var activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
-        assertEquals("taking a shower for 2 minutes", activity.displayActivity());
-
+        Activity activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        assertEquals("taking a shower for 2 minutes", Activity.displayActivity(activity.title));
     }
 
     @Test
     void testEquals() {
-        var activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
-        var activity2 = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity2 = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
         assertEquals(activity, activity2);
     }
 
     @Test
     void testNotEquals() {
-        var activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
-        var activity2 = new Activity("1","/path/to/image/","Taking a shower for 5 minutes", 200);
+        Activity activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity2 = new Activity("1","/path/to/image/","Taking a shower for 5 minutes", 200);
         assertNotEquals(activity, activity2);
     }
 
     @Test
     void testHashCode() {
-        var activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
-        var activity2 = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity2 = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
         assertEquals(activity, activity2);
         assertEquals(activity.hashCode(), activity2.hashCode());
     }
 
     @Test
     void testToString() {
-        var activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
+        Activity activity = new Activity("1","/path/to/image/","Taking a shower for 2 minutes", 200);
         assertEquals(ToStringBuilder.reflectionToString(activity, MULTI_LINE_STYLE), activity.toString());
     }
 }
