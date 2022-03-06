5fe37f107a562185641f03c062b8f4cd0402b65d
Tadas Lukavicius <T.lukavicius@student.tudelft.nl>
2022-03-06 15:28:13 +0100

    Added tests

---
+++ commons/src/test/java/commons/PlayerTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void checkConstructorAndGetPoints() {
+        Player p = new Player();
+        assertEquals(p.getPoints(), 0);
+    }
+
+    @Test
+    public void checkGetAndSetUsername() {
+        Player p = new Player();
+        p.setUsername("blabla");
+        assertEquals(p.getUsername(), "blabla");
+    }
+
+    @Test
+    public void checkIncreaseAndDecreasePoints() {
+        Player p = new Player();
+        p.setUsername("blabla");
+        p.increasePoints(20);
+        p.decreasePoints(5);
+        assertEquals(p.getPoints(), 15);
+    }
+
+    @Test
+    public void checkEqualsNull() {
+        Player p = new Player();
+        assertFalse(p.equals(null));
+    }
+
+    @Test
+    public void checkEqualsTrue() {
+        Player p = new Player();
+        p.setUsername("blabla");
+        Player b = new Player();
+        b.setUsername("blabla");
+        assertTrue(b.equals(p));
+    }
+
+    @Test
+    public void checkEqualsFalse() {
+        Player p = new Player();
+        p.setUsername("blabla");
+        Player b = new Player();
+        b.setUsername("blabla");
+        b.increasePoints(1);
+        assertFalse(p.equals(b));
+    }
+
+}
