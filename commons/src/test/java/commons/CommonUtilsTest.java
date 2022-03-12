a7b13a4bc75d66c02841f5903f914f851d8e8e6e
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2022-03-12 09:22:58 +0100

    Fixed checkstyle, JavaDoc needed for @BeforeAll
    Because JavaDoc was needed for @BeforeAll, the pipeline failed. Removed the setup method, because it was not really necessary anyways.

--- commons/src/test/java/commons/CommonUtilsTest.java
+++ commons/src/test/java/commons/CommonUtilsTest.java
@@ -1,6 +1,5 @@
 package commons;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -8,25 +7,21 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommonUtilsTest {
-    private static CommonUtils utils;
-
-    @BeforeAll
-    public static void setup(){
-        utils = new CommonUtils();
-    }
-
     @Test
     void prependingZeroTestPrepend(){
+        CommonUtils utils = new CommonUtils();
         assertEquals("01", utils.addPrependingZero(1));
     }
 
     @Test
     void prependingZeroTestNoPrepend(){
+        CommonUtils utils = new CommonUtils();
         assertEquals("10", utils.addPrependingZero(10));
     }
 
     @Test
     void randomIntInRangeTestNegative(){
+        CommonUtils utils = new CommonUtils();
         int randomNegative = utils.randomIntInRange(-50, -10, new Random(12345));
         Random r = new Random(12345);
         assertEquals((r.nextInt(60) - 10) * -1, randomNegative);
@@ -34,6 +29,7 @@ public class CommonUtilsTest {
 
     @Test
     void randomIntInRangeTestPositive(){
+        CommonUtils utils = new CommonUtils();
         int randomPositive = utils.randomIntInRange(10, 50, new Random(12345));
         Random r = new Random(12345);
         assertEquals(r.nextInt(60) - 10, randomPositive);
