e6cffedbbb76e9595d1cbca443ca71bc5593c327
Gijs van der Heide <G.vanderHeide-1@student.tudelft.nl>
2022-03-13 23:24:46 +0100

    Added equals, hashCode, toString for GameUpdate as well as tests

--- commons/src/main/java/commons/gameupdate/GameUpdate.java
+++ commons/src/main/java/commons/gameupdate/GameUpdate.java
@@ -1,6 +1,11 @@
 package commons.gameupdate;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 /**
  * A GameUpdate represents a change in the state of the current game, and is sent to clients to inform them
@@ -10,6 +15,32 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 public abstract class GameUpdate {
 
+    /**
+     * Checks if 2 game update objects are equal
+     * @param obj the object that will be compared
+     * @return true or false, whether the objects are equal or not
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
 
+    /**
+     * Generate a hash code for this object
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * Creates a formatted string for this object
+     * @return a formatted string in multi line style
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
 
 }
