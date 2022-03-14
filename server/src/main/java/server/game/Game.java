b930df932ee683ab2178e84bd40cdbf5cc9b7590
Gijs van der Heide <G.vanderHeide-1@student.tudelft.nl>
2022-03-14 09:36:31 +0100

    Added tests for Game

--- server/src/main/java/server/game/Game.java
+++ server/src/main/java/server/game/Game.java
@@ -1,11 +1,16 @@
 package server.game;
 
 import commons.Player;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 public class Game {
 
@@ -108,6 +113,34 @@ public class Game {
 
         return this.players.containsKey(username);
 
+    }
+
+    /**
+     * Checks if 2 game objects are equal
+     * @param obj the object that will be compared
+     * @return true or false, whether the objects are equal or not
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    /**
+     * Generate a hash code for this object
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * Creates a formatted string for this object
+     * @return a formatted string in multi line style
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
     }
 
 }
