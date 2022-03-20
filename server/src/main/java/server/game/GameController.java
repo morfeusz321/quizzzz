ceb595c17febb5112c5c4f98e60423869ce9fcf1
Gijs van der Heide <G.vanderHeide-1@student.tudelft.nl>
2022-03-20 15:42:08 +0100

    Refactor API game methods
    The Game API methods have been moved to a separate controller.

--- server/src/main/java/server/game/GameController.java
+++ server/src/main/java/server/game/GameController.java
@@ -2,19 +2,12 @@ package server.game;
 
 import commons.GameType;
 import commons.Player;
-import commons.Question;
 import commons.gameupdate.GameUpdate;
 import commons.gameupdate.GameUpdateFullPlayerList;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
 import server.api.ScoreController;
 
 import javax.annotation.PostConstruct;
@@ -25,8 +18,6 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@RestController
-@RequestMapping("/api/game")
 public class GameController implements ApplicationContextAware {
 
     private ConcurrentHashMap<UUID, Game> runningGames;
@@ -232,69 +223,6 @@ public class GameController implements ApplicationContextAware {
         if(game != currentGame && game.getPlayers().size() == 0){
             stopGame(game);
         }
-
-    }
-
-    /**
-     * Maps to /game/questions?gameID=id
-     * Returns the list of questions of a Game if there is one that has the corresponding UUID
-     *
-     * @return 200 OK: List of all questions,
-     * 400 Bad Request if the UUID is malformed or there is no running game with it
-     * 500 Internal Server Error if no question(s) could be generated and the game does not have 20 questions
-     */
-    @GetMapping("/questions")
-    public ResponseEntity<List<Question>> getAllQuestions(@RequestParam("gameID") String gameIDString){
-        UUID uuid;
-        try {
-            uuid = UUID.fromString(gameIDString);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Game game = this.getGame(uuid);
-
-        if(game == null || game == currentGame) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        if(game.getQuestions() == null || game.getQuestions().size() != 20){
-            return ResponseEntity.internalServerError().build();
-        }
-
-        return ResponseEntity.ok(game.getQuestions());
-    }
-
-    /**
-     * Provides game loop updates in the form of a deffered result to allow for
-     * long polling to this endpoint. Maps to api/game/
-     * @param gameIDString the UUID of the game whose updates the client wishes to
-     *                     subscribe to
-     * @return 200 OK: game loop update, or bad request if the game UUID does not exist.
-     * Can also return an internal server error in case of timeout.
-     */
-    @GetMapping("/")
-    public DeferredResult<ResponseEntity<String>> gameLongPollLoop(@RequestParam("gameID") String gameIDString) {
-
-        DeferredResult<ResponseEntity<String>> result = new DeferredResult<>(20000L, ResponseEntity.internalServerError().build());
-
-        UUID uuid;
-        try {
-            uuid = UUID.fromString(gameIDString);
-        } catch (IllegalArgumentException e) {
-            result.setResult(ResponseEntity.badRequest().build());
-            return result;
-        }
-
-        Game game = this.getGame(uuid);
-
-        if(game == null || game == currentGame) {
-            result.setResult(ResponseEntity.badRequest().build());
-            return result;
-        }
-
-        game.runDeferredResult(result);
-        return result;
 
     }
 
