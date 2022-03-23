ab7ed1228aa9ed056ae1ae25a68f647fccbedccc
Gijs van der Heide <G.vanderHeide-1@student.tudelft.nl>
2022-03-23 13:49:42 +0100

    Added 2 new GameUpdates

---
+++ commons/src/main/java/commons/gameupdate/GameUpdateDisplayLeaderboard.java
@@ -0,0 +1,43 @@
+package commons.gameupdate;
+
+import commons.Score;
+
+import java.util.List;
+
+/**
+ * This GameUpdate is sent to a client to inform it of the fact that it should be displaying the leaderboard
+ * that divides the game into two phases
+ */
+public class GameUpdateDisplayLeaderboard {
+
+    private List<Score> leaderboard;
+
+    /**
+     * Empty constructor used by object mapper
+     */
+    @SuppressWarnings("unused")
+    private GameUpdateDisplayLeaderboard() {
+
+    }
+
+    /**
+     * Creates a new GameUpdate
+     * @param leaderboard the leaderboard to be displayed by the client
+     */
+    public GameUpdateDisplayLeaderboard(List<Score> leaderboard) {
+
+        this.leaderboard = leaderboard;
+
+    }
+
+    /**
+     * Returns the leaderboard to be displayed by the client
+     * @return the leaderboard to be displayed by the client
+     */
+    public List<Score> getLeaderboard() {
+
+        return this.leaderboard;
+
+    }
+
+}
