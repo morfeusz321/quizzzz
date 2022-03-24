a5b6c24770fd77c39b4896621bec82d72187b485
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2022-03-24 15:36:32 +0100

    Remove unused import

--- server/src/test/java/server/game/FakeApplicationContext.java
+++ server/src/test/java/server/game/FakeApplicationContext.java
@@ -1,6 +1,5 @@
 package server.game;
 
-import commons.Activity;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
