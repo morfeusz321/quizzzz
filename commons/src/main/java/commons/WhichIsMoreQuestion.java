6827998c1bbfcc25d1b9a2938b58dcbd6f82c5c6
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2022-03-25 17:29:04 +0100

    Adjust constructor of WhichIsMoreQuestion
    Adjusted the constructor of the WhichIsMoreQuestion, as the imagePath is needed for displaying the answer. Setting the title too, makes checking for equality easier.

--- commons/src/main/java/commons/WhichIsMoreQuestion.java
+++ commons/src/main/java/commons/WhichIsMoreQuestion.java
@@ -23,11 +23,12 @@ public class WhichIsMoreQuestion extends Question{
      * @param answer the answer to this question (i.e. the index 1, 2, 3 of the multiple choice answer)
      */
     public WhichIsMoreQuestion(List<Activity> answerOptions, long answer) {
-        // Note: we can assume that the question is initialized with non-null parameters. Questions are only initialized
-        // in the QuestionGenerator (server-side), where null-checks are done to make that sure.
+        // Note: we can assume that the question is initialized with non-null and valid parameters. Questions are only
+        // initialized in the QuestionGenerator (server-side), where null-checks are done to make that sure.
 
-        this.activityTitle = null;
-        this.activityImagePath = null;
+        Activity mainActivity = answerOptions.get((int) answer - 1); // starts from 1
+        this.activityTitle = mainActivity.title;
+        this.activityImagePath = mainActivity.imagePath;
 
         this.answerOptions = new ArrayList<>();
         for(Activity a : answerOptions) {
