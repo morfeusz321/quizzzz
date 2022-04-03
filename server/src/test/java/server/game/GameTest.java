c75c176869463fc90c97e28e4ea0f717fb3dacfe
Tereza Ležovičová <t.lezovicova@student.tudelft.nl>
2022-04-03 16:50:42 +0200

    Fixed checkstyle

--- server/src/test/java/server/game/GameTest.java
+++ server/src/test/java/server/game/GameTest.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
