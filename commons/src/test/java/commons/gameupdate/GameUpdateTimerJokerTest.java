6cb7be6a536f543260bcf50284d630a950371fd3
Filip <F.J.Blaszczyk@student.tudelft.nl>
2022-04-06 23:13:42 +0200

    Added GameUpdateTests

---
+++ commons/src/test/java/commons/gameupdate/GameUpdateTimerJokerTest.java
@@ -0,0 +1,26 @@
+package commons.gameupdate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameUpdateTimerJokerTest {
+
+    @Test
+    void testConstructor(){
+        ConcurrentHashMap<String,Long> temp = new ConcurrentHashMap<>();
+        GameUpdateTimerJoker gameUpdateTimerJoker = new GameUpdateTimerJoker(temp);
+        Assertions.assertNotNull(gameUpdateTimerJoker);
+    }
+
+    @Test
+    void getTime() {
+        ConcurrentHashMap<String,Long> temp = new ConcurrentHashMap<>();
+        GameUpdateTimerJoker gameUpdateTimerJoker = new GameUpdateTimerJoker(temp);
+        Assertions.assertEquals(gameUpdateTimerJoker.getTime(),temp);
+    }
+}
